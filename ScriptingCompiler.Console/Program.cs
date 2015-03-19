c44c029670e8f6f7715c4c5ae9b715f462c8a5a3
manueldiego <chico_herbal@hotmail.com>
2015-03-19 01:53:20 +0100

    First deploy

---
+++ ScriptingCompiler.Console/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TheHunter.Scripting;
+
+namespace ScriptingCompiler.Cmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ScriptHosting scripHost = new ScriptHosting();
+            
+            Console.WriteLine(@"Welcome to C# REPL. write quit to exit at any time.");
+            while (true)
+            {
+                string code = Console.ReadLine();
+                if (code != null && code.ToLower() != "quit")
+                {
+                    try
+                    {
+                        object result = scripHost.Execute(code);
+                        Console.WriteLine(@"	{0}", result);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ ScriptingCompiler/ScriptHosting.cs
@@ -0,0 +1,53 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Roslyn.Scripting;
+using Roslyn.Scripting.CSharp;
+
+namespace TheHunter.Scripting
+{
+    
+    public class ScriptHosting
+    {
+        private readonly ScriptEngine scriptEngine;
+        private readonly Session session;
+
+        public ScriptHosting(IEnumerable<string> namespaces = null, IEnumerable<Assembly> assemblies = null)
+        {
+            List<Assembly> references = new List<Assembly>() {   
+                                    typeof(IEnumerable<>).Assembly,  
+                                    typeof(IQueryable).Assembly,
+                                    typeof(IQueryable<>).Assembly,
+                                    typeof(List<>).Assembly };
+            List<string> ns = new List<string>() { "System", "System.Collections", "System.Collections.Generic", "System.Text" };
+
+            if (assemblies != null)
+                references.AddRange(assemblies);
+
+            if (namespaces != null)
+                ns.AddRange(namespaces);
+
+            this.scriptEngine = new ScriptEngine();
+            references.ForEach(assembly => scriptEngine.AddReference(assembly));
+            ns.ForEach(s => scriptEngine.ImportNamespace(s));
+
+            this.session = scriptEngine.CreateSession(this);
+        }
+
+        public object Execute(string code)
+        {
+            return session.Execute(code);
+        }
+
+        public T Execute<T>(string code)
+        {
+            return session.Execute<T>(code);
+        }
+
+        public void ExecuteFile(string path)
+        {
+            session.ExecuteFile(path);
+        }
+    }
+}
